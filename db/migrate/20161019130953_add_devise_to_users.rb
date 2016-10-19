6d1735b5442ee3233ec95de98dfc26d859241b46
Emily Au-Young <em.auyoung@gmail.com>
2016-10-19 21:15:29 +0800

    db migration

--- db/migrate/20161019130953_add_devise_to_users.rb
+++ db/migrate/20161019130953_add_devise_to_users.rb
@@ -2,6 +2,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019121654) do
+ActiveRecord::Schema.define(version: 20161019130953) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -22,8 +23,8 @@ ActiveRecord::Schema.define(version: 20161019121654) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index [nil], name: "index_users_on_email", unique: true
   end
 
 end
